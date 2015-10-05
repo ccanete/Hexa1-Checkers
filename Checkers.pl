0d7ec5b4fde529c0de934bbf93bcc103ed1023be
Adrien Menella <adrien.menella@gmail.com>
2015-10-05 11:00:14 +0200

    modification initialisation (changement noms + ajout légendes)

--- Checkers.pl
+++ Checkers.pl
@@ -2,6 +2,22 @@
 %   Prolog game                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-% The initial board
-init(Board) :-
-      Board = b(n,x,n,x,n,x,n,x,x,n,x,n,x,n,x,n,n,x,n,x,n,x,n,x,e,n,e,n,e,n,e,n,n,e,n,e,n,e,n,e,o,n,o,n,o,n,o,n,n,o,n,o,n,o,n,o,o,n,o,n,o,n,o,n).
+% The initial board (origin box : lower left corner of the board)
+init( Board) :-
+      Board = b(wp,n,wp,n,wp,n,wp,n,wp,n,
+				n,wp,n,wp,n,wp,n,wp,n,wp,
+				wp,n,wp,n,wp,n,wp,n,wp,n,
+				n,wp,n,wp,n,wp,n,wp,n,wp,
+				e,n,e,n,e,n,e,n,e,n,
+				n,e,n,e,n,e,n,e,n,e,
+				bp,n,bp,n,bp,n,bp,n,bp,n,
+				n,bp,n,bp,n,bp,n,bp,n,bp,
+				bp,n,bp,n,bp,n,bp,n,bp,n,
+				n,bp,n,bp,n,bp,n,bp,n,bp).
+
+% n : null (unaccessible box)
+% e : free box
+% bq : black queen
+% wq : white queen
+% bp : black pawn
+% wp : white pawn
