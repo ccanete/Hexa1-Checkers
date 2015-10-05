07b3599de4e318e73d9cf18a33169febfcf472f7
ccanete <cyril.canete@insa-lyon.fr>
2015-10-05 10:20:02 +0200

    initial commit with complete code

---
+++ Checkers.pl
@@ -0,0 +1,7 @@
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%   Prolog game                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% The initial board
+init(Board) :-
+      Board = b(n,x,n,x,n,x,n,x,x,n,x,n,x,n,x,n,n,x,n,x,n,x,n,x,e,n,e,n,e,n,e,n,n,e,n,e,n,e,n,e,o,n,o,n,o,n,o,n,n,o,n,o,n,o,n,o,o,n,o,n,o,n,o,n).
