9a4fbcc7853ae517b47b82e0a774271e40f31f41
V1C0D3R <V1C0D3R@gmail.com>
2015-10-12 17:41:31 +0200

    Add 2 new modules : Action & DrawBoard

---
+++ Action.pl
@@ -0,0 +1,21 @@
+
+% Process Move after having check rules
+processMove(Board, X, Y, NewX, NewY, NewBoard) :-
+  convertCoordinate(X, Y, Pos),
+  %write('Pos : '),
+  %write(Pos), nl,
+  convertCoordinate(NewX, NewY, NewPos),
+  %write('NewPos : '),
+  %write(NewPos), nl,
+  nth0(Pos, Board, Piece),
+  replace(Board, Pos, em, TempBoard),
+  replace(TempBoard, NewPos, Piece, NewBoard).
+
+% Not functionnal
+processEat(Board, X, Y, NewX, NewY, NewBoard) :-
+  XEaten is (X+NewX)/2,
+  YEaten is (Y+NewY)/2,
+  convertCoordinate(XEaten, YEaten, Pos),
+  replace(Board, Pos, em, NewBoard).
+
+  
