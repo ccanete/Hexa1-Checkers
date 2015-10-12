14e4e56bd53cb441ec378093af7537b6bdc0cfa5
V1C0D3R <V1C0D3R@gmail.com>
2015-10-12 12:02:39 +0200

    Add modularity for doQueen and board is now dynamic #noparams add Queen.pl

---
+++ Queen.pl
@@ -0,0 +1,27 @@
+%% DO QUEEN %%
+doQueen(Board, NewBoard, NewX, NewY):-
+  %NewBoard is Board,
+  checkQueen(Board, NewX, NewY),
+  becameQueen(Board, NewBoard, NewX, NewY),!.
+doQueen(Board, Board, _, _).
+
+% params :
+checkQueen(Board, NewX, NewY):-
+  getPiece(Board, NewX, NewY, Piece),
+  checkQueen(Board, NewX, NewY, Piece).
+checkQueen(Board, NewX, NewY, bp):-
+    NewY = 1.
+checkQueen(Board, NewX, NewY, wp):-
+    NewY = 10.
+
+% Predicate became queen (call it between turns not replays)
+becameQueen(Board, NewBoard, NewX, NewY) :-
+  convertCoordinate(NewX, NewY, NewPos),
+  getPiece(Board, NewX, NewY, P),
+  convertQueen(P, Q),
+  replace(Board, NewPos, Q, NewBoard).
+becameQueen(Board, Board, _, _).
+
+%Convert to queen
+convertQueen(bp,bq).
+convertQueen(wp,wq).
