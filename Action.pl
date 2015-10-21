7c5415c3f3cde63148922f00a06213b0e85143cf
ccanete <cyril.canete@insa-lyon.fr>
2015-10-21 17:36:02 +0200

    add comment

--- Action.pl
+++ Action.pl
@@ -1,4 +1,4 @@
-
+%% MOVE %%
 % Process Move after having check rules
 processMove(Board, X, Y, NewX, NewY, NewBoard) :-
   convertCoordinate(X, Y, Pos),
@@ -11,11 +11,10 @@ processMove(Board, X, Y, NewX, NewY, NewBoard) :-
   replace(Board, Pos, em, TempBoard),
   replace(TempBoard, NewPos, Piece, NewBoard).
 
+%% EAT %%
 % Not functionnal
 processEat(Board, X, Y, NewX, NewY, NewBoard) :-
   XEaten is (X+NewX)/2,
   YEaten is (Y+NewY)/2,
   convertCoordinate(XEaten, YEaten, Pos),
   replace(Board, Pos, em, NewBoard).
-
-  
