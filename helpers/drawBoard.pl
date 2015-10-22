ff37803647b1ff9e900c77fd7c8ef3e94b9bdc48
ccanete <cyril.canete@insa-lyon.fr>
2015-10-22 11:26:33 +0200

    use new coordinate in print board

--- helpers/drawBoard.pl
+++ helpers/drawBoard.pl
@@ -7,10 +7,10 @@ printBoard(Board) :-
   write('|--+--+--+--+--+--+--+--+--+--+--|'), nl,
   printBoard(Board, 1), !.
 % Calls the PrintLine function and iterates
-printBoard(Board, Line) :-
-  printLine(Board, Line),
+printBoard(Board, Y) :-
+  printLine(Board, Y),
   printGrid,
-  NextLine is Line + 1,
+  NextLine is Y + 1,
   printBoard(Board, NextLine).
 % End of the loop
 printBoard( _, 11) :-
@@ -21,21 +21,21 @@ printGrid :-
   write('|--+--+--+--+--+--+--+--+--+--+--|'), nl.
 
 % Start printing a line recursivly (loop style)
-printLine(Board, Line) :-
+printLine(Board, Y) :-
    write('|'),
-   printLine(Board, Line, 0).
+   printLine(Board, Y, 0).
 % Print a piece of the line then recursiv call
-printLine(Board, Line, 0) :-
-  write(Line),
+printLine(Board, Y, 0) :-
+  write(Y),
   write(' |'),
-  printLine(Board, Line, 1).
-printLine(Board, Line, Col) :-
-  getPiece(Board, Line, Col, Piece),
+  printLine(Board, Y, 1).
+printLine(Board, Y, X) :-
+  getPiece(Board, X, Y, Piece),
   pieceToSymbol(Piece, Symbol),
   write(Symbol),
   write('|'),
-  NextCol is Col + 1,
-  printLine(Board, Line, NextCol).
+  NextCol is X + 1,
+  printLine(Board, Y, NextCol).
 % End of the loop
 printLine( _, _, 11) :- nl,!.
 
