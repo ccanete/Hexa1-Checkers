939439054d1d4a568620f44fe2e6e7493648fae4
ccanete <cyril.canete@insa-lyon.fr>
2015-10-22 12:31:21 +0200

    end of clean code

--- main.pl
+++ main.pl
@@ -28,23 +28,19 @@ initGame:-
   %% test eat and move
   write('--- GAME 1 ---'),nl,
   printBoard(Board),
-  play(Board, 4, 2, 2, 4, white),
-  write('--- GAME 2 ---'),nl,
-  printBoard(Board),
-  play(Board, 1, 1, 2, 10, white).
+  play(Board, 4, 4, 5, 5, white),
 
 %play(Board, X, Y, NewX, NewY, Color):- gameover, !.
 play(Board, X, Y, NewX, NewY, Color):-
-  checkMove(Board, X, Y, NewX, NewY, Color),
-  processMove(Board, X, Y, NewX, NewY, BoardAfterMove),
+  doMove(Board, X, Y, NewX, NewY, BoardAfterMove),
   printBoard(BoardAfterMove),
   % BE CAREFULL, IF NO EAT WHAT BOARD SHOULD BE USED ?
   %checkEat(Board, X, Y, NewX, NewY),
-  processEat(BoardAfterMove, X, Y, NewX, NewY, BoardAfterEat),
-  printBoard(BoardAfterEat),
-  doQueen(BoardAfterEat, BoardAfterQueen, NewX, NewY),
-  printBoard(BoardAfterQueen),
-  continuePlaying(BoardAfterQueen),
+  %processEat(BoardAfterMove, X, Y, NewX, NewY, BoardAfterEat),
+  %printBoard(BoardAfterEat),
+  %doQueen(BoardAfterEat, BoardAfterQueen, NewX, NewY),
+  %printBoard(BoardAfterQueen),
+  %continuePlaying(BoardAfterQueen),
   write('Play again !').
 
 % The initial board (origin box : lower left corner of the board)
