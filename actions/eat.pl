628b128d2f92e7f2074a7c61d656fdce0cbcf9d1
anaoprisor <anaoprisor77@gmail.com>
2015-10-23 08:39:19 +0200

    integration doEat() with checkEat and processEat performing actual move

--- actions/eat.pl
+++ actions/eat.pl
@@ -2,22 +2,27 @@
 
 %% DO EAT
 
+:-['../helpers/util.pl']. % fichier mis à part, car peut etre utile pour d.autres actions
 
-% TODO : remplacer tous les zombies par des cases vides à la fin d'un tour
+
+% TODO : remplacer tous les zombies par des cases vides à la fin d.un tour
 doEat(Board, X, Y, NewX, NewY, NewBoard) :-
-checkEat(Board, X, Y, XEaten, YEaten, NewX, NewY),
-processEat(Board, XEaten, YEaten, NewBoard).
+checkEat(Board, X, Y, XEaten,YEaten, NewX, NewY),
+convertCoordinate(X, Y, PosEater),
+convertCoordinate(XEaten, YEaten, PosEaten),
+convertCoordinate(NewX, NewY, NewPos),
+processEat(Board, PosEater, PosEaten, NewPos, NewBoard).
 
 %%END OF DO EAT
 
 
 %% PROCESS EAT
 
-processEat(Board, XEaten, YEaten, NewBoard) :-
-	convertCoordinate(XEaten, YEaten, Pos),
-	% on remplace la piece par un zombie
-  replace(Board, Pos, zb, NewBoard).
-
+processEat(Board, PosEater, PosEaten, NewPos, NewBoard) :- 
+findPiece(Board, PosEater, EaterPiece),
+replace(Board, PosEater, em, NewBoardIntention),
+replace(NewBoardIntention, PosEaten, em, NewBoardDigestion),
+replace(NewBoardDigestion, NewPos, EaterPiece, NewBoard). 
 
 /* old processEat
 
@@ -56,33 +61,46 @@ findEatenPiece(Board, X, Y, DeltaX, DeltaY, XEaten, YEaten) :-
 
 
 
-%% CHECK EAT
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% CHECK EAT
 
-% CheckEat for coordinates
+% Fonction principale checkEat prend en compte les coordonnées
+
+% ----------------------------------- CheckEat for coordinates
+% L.ordre d.évaluation  des règles .est pas important si on ne fornit pas certains paramètres,
+% Prolog va évaluer toutes les règles avec toutes les configurations possibles des valeurs pour voir s il peut rendre vrai l.ensemble des prédicats
+
+%  règle qui converti les coordonnées en positions pour pouvoir reutiliser la fonction principale
+
+
 checkEat(Board, PosXEater, PosYEater, PosXEaten, PosYEaten, PosXNextCase, PosYNextCase) :-
-  convertCoordinate(PosXEater, PosYEater, PosEater),
+  convertCoordinate(PosXEater, PosYEater, PosEater), 
   convertCoordinate(PosXEaten, PosYEaten, PosEaten),
   checkEat(Board, PosEater, PosEaten, PosNextCase),
   convertCoordinate(PosXNextCase, PosYNextCase, PosNextCase).
 
-% Check for given Pieces positions
+
+%  ----------------------------------- Check for given Pieces positions
 % TODO : Optimise algorithm to let Prolog find (unify) all eater and eatable piece configurations on Board 
+% ou sinon forcer Prolog à le faire en évaluant les règles d.IA pour obtenir le meilleur des cas 
+
 checkEat(Board, PosEater, PosEaten, LandingPosCandidate) :-
   validPlayerPosition(Board, PosEater, PosEaten, EaterPiece, EatenPiece),
 
-  % If queen, range will be allowed
+  % -----------------------------------If queen, range will be allowed
   getPieceRange(EaterPiece, EaterRangeFactor),
   
-  % Check if eating is feasible
+  % -----------------------------------Check if eating is feasible
   checkNearPiecesToEat(Board, PosEater, PosEaten, EaterRangeFactor, LandingPosCandidate),
   checkFreeWayToEat(Board, LandingPosCandidate).
+
 
 validPlayerPosition(Board, PosEater, PosEaten, EaterPiece, EatenPiece) :-
   between(0, 99, PosEater), between(0, 99, PosEaten),  PosEater - PosEaten \= 0,
   
-  % Find pieces that can be used for eating
+  %  Find pieces that can be used for eating (eatable)
   findPiece(Board, PosEater, EaterPiece),
   findPiece(Board, PosEaten, EatenPiece),
+  % check if opposite
   checkOpositePlayablePieces(EaterPiece, EatenPiece).
 
 findPiece(Board, PosPiece, Piece) :-
@@ -98,10 +116,13 @@ getPieceRange(Piece, RangeFactor) :-
   isQueen(Piece), RangeFactor is 10;
   isPawn(Piece), RangeFactor is 1.
 
+% START ----------------------------------- checkNearPiecesToEat
+
 % We check for nearest pieces suitable for eating on the Diagonal lines and within the piece eating range
+% 
 checkNearPiecesToEat(Board, PosEater, PosEaten, EaterRangeFactor, LandingPosCandidate) :-
    % Eaten is on eaters diagonals
-  haveExactLinesToTarget(PosEater, PosEaten, LinesToTarget, DiagonalStepDistance),
+  piecesAreOnTheSameDiagonal(PosEater, PosEaten, LinesToTarget, DiagonalStepDistance),
   % Eaters Range is enough to reach eaten piece
   EaterRangeFactor >= abs(LinesToTarget), 
 
@@ -114,7 +135,11 @@ checkNearPiecesToEat(Board, PosEater, PosEaten, EaterRangeFactor, LandingPosCand
   % Case to land eater after eating
   LandingPosCandidate is PosEater - DiagonalStepDistance * LinesToNewPosition.
 
-haveExactLinesToTarget(PosEater, PosEaten, LinesToTarget, DiagonalStepDistance) :-
+% END ----------------------------------- checkNearPiecesToEat
+
+% START ----------------------------------- piecesAreOnTheSameDiagonal
+
+piecesAreOnTheSameDiagonal(PosEater, PosEaten, LinesToTarget, DiagonalStepDistance) :-
   % Evaluate PosEater regarding a (+/-) 9 or 11 Diagonal Range Factor
   diagonalStepDistance(DiagonalStepDistance),
 
@@ -122,12 +147,23 @@ haveExactLinesToTarget(PosEater, PosEaten, LinesToTarget, DiagonalStepDistance) 
   LinesToTarget is div(PosEater - PosEaten, DiagonalStepDistance),
   0 is mod(PosEater - PosEaten, DiagonalStepDistance).
 
+  % END ----------------------------------- piecesAreOnTheSameDiagonal
+
 diagonalStepDistance(11). % For up left and down right case
 diagonalStepDistance(9). % For up right and down left case
+
+% ----------------------------------- getLinesToNewPosition
+% règle qui calcule la prochaine case sur la diagonale, prennant en compte la position précédente  pour lui donner une direction
 
 getLinesToNewPosition(LinesToTarget, LinesToNewPosition) :- 
   LinesToTarget > 0, LinesToNewPosition is LinesToTarget + 1; % Get lower line on the Diagonal direction
   LinesToTarget < 0, LinesToNewPosition is LinesToTarget - 1. % Get upper line on the Diagonal direction
+
+% ----------------------------------- noPiecesBetween 
+% On itère sur toutes les positions sur la diagonale entre la case de départ et la case de la pièce à manger 
+% pour vérifier qu.elles sont libres pour les passages notemment de la reine
+% Dans le cas du pawn, la 1ère règle sera valide par défaut car il n.y a pas de distance superieure à 1 entre les 2 pièces
+% et on n.évalue pas le reste, du coup
 
 noPiecesBetween(Board, PosEater, LinesToTarget, DiagonalStepDistance, CurrentLine) :-
   LinesToTarget is CurrentLine;
