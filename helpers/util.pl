e9ee640bbae937bdba38a70b24436198322c096e
ccanete <cyril.canete@insa-lyon.fr>
2015-10-31 12:21:28 +0100

    refactor turn circle

--- helpers/util.pl
+++ helpers/util.pl
@@ -15,22 +15,6 @@ checkBoarders(X, Y) :-
   between(1, 10, X),
   between(1, 10, Y).
 
-%% OLD convertCoordinate code
-/*
-% Convert coordinate to array index (index starts at 1)
-convertCoordinate(Line, Column, Pos):-
-  %write('Line : '),
-  %write(Line), nl,
-  %write('Column : '),
-  %write(Column), nl,
-  Line =< 10,
-  Line >= 1,
-  Column >= 1,
-  Column =< 10,
-  Pos is (Line-1) * 10 + Column-1.
-*/
-
-
 % Replace an element in an array. (Board, Index, NexElement, NewBoard)
 replace([_|T], 0, X, [X|T]).
 replace([H|T], I, X, [H|R]):- I > -1, NI is I-1, replace(T, NI, X, R), !.
--- main.pl
+++ main.pl
@@ -2,18 +2,6 @@
 %           Checkers game             %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-dynamic board/1.
-board([wp,nl,wp,nl,wp,nl,wp,nl,wp,nl,
-               nl,wp,nl,wp,nl,wp,nl,wp,nl,wp,
-               wp,nl,wp,nl,wp,nl,wp,nl,wp,nl,
-               nl,wp,nl,wp,nl,wp,nl,wp,nl,wp,
-               em,nl,em,nl,em,nl,em,nl,em,nl,
-               nl,em,nl,em,nl,em,nl,em,nl,em,
-               bp,nl,bp,nl,bp,nl,bp,nl,bp,nl,
-               nl,bp,nl,bp,nl,bp,nl,bp,nl,bp,
-               bp,nl,bp,nl,bp,nl,bp,nl,bp,nl,
-               nl,bp,nl,bp,nl,bp,nl,bp,nl,bp]).
-
 /* Module Imports */
 ?- ['actions/queen.pl'].
 ?- ['actions/eat.pl'].
@@ -21,6 +9,7 @@ board([wp,nl,wp,nl,wp,nl,wp,nl,wp,nl,
 ?- ['actions/checkEat.pl'].
 ?- ['helpers/drawBoard.pl'].
 ?- ['helpers/util.pl'].
+?- ['helpers/turn.pl'].
 
 % Main function
 playCheckers:-
@@ -29,13 +18,20 @@ playCheckers:-
   play(Board, white).
 
 play(Board, Player):-
-  %TODO: check games end
+  continuePlaying(Board),
   write('Player '), write(Player), write(' plays.'),nl,
   getUserMove(X,Y,NewX,NewY),
   write('Move: X='), write(X), write(', Y='), write(Y), write(', NewX='), write(NewX), write(', NewY='), write(NewY),nl,
   processTurn(Board, Player, X, Y, NewX, NewY, NewBoard),
   nextPlayer(Player, NextPlayer),
   play(NewBoard, NextPlayer).
+  %TODO: handle a wrong turn
+
+play(Board, Player):-
+  %GameOver for a player
+  not(continuePlaying(Board)),
+  %TODO: Find who has won
+  write('GameOver').
 
 %play(Board, X, Y, NewX, NewY, Color):- gameover, !.
 processTurn(Board, Player, X, Y, NewX, NewY, BoardAfterQueen):-
@@ -45,45 +41,3 @@ processTurn(Board, Player, X, Y, NewX, NewY, BoardAfterQueen):-
   printBoard(BoardAfterEat),
   doQueen(BoardAfterEat, BoardAfterQueen, NewX, NewY),
   printBoard(BoardAfterQueen).
-
-% The initial board (origin box : lower left corner of the board)
-initBoard(Board) :-
-      Board = [wp,nl,wp,nl,wp,nl,wp,nl,wp,nl,
-      				 nl,wp,nl,wp,nl,wp,nl,wp,nl,wp,
-      				 wp,nl,wp,nl,wp,nl,wp,nl,wp,nl,
-      				 nl,wp,nl,wp,nl,wp,nl,wp,nl,wp,
-      				 em,nl,em,nl,em,nl,em,nl,em,nl,
-      				 nl,em,nl,em,nl,em,nl,em,nl,em,
-      				 bp,nl,bp,nl,bp,nl,bp,nl,bp,nl,
-      				 nl,bp,nl,bp,nl,bp,nl,bp,nl,bp,
-      				 bp,nl,bp,nl,bp,nl,bp,nl,bp,nl,
-      				 nl,bp,nl,bp,nl,bp,nl,bp,nl,bp].
-
-% nl : null (unaccessible box)
-% em : free box
-% bq : black queen
-% wq : white queen
-% bp : black pawn
-% wp : white pawn
-
-getUserMove(X,Y,NewX,NewY):-
-  write('Enter the line number of the pawn you want to move (Y)'),nl,
-  read(Y),
-  write('Enter the column number of the pawn you want to move (X)'),nl,
-  read(X),
-  write('Enter the line number of the box you want to move to (NewY)'),nl,
-  read(NewY),
-  write('Enter the column number of the box you want to move to (NewX)'),nl,
-  read(NewX).
-
-nextPlayer(white,black).
-nextPlayer(black, white).
-
-% Check if a player has won
-continuePlaying(Board):-
-  continuePlaying(Board, white),
-  continuePlaying(Board, black).
-continuePlaying(Board, white):-
-  member(wp, Board),!;member(wq, Board),!.
-continuePlaying(Board, black):-
-  member(bp, Board),!;member(bq, Board),!.
