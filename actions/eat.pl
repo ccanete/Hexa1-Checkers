d3381de7f57e9261e2a94475716daadf8ffad0ba
ccanete <cyril.canete@insa-lyon.fr>
2015-10-31 14:14:34 +0100

    quick fix syntax

--- actions/eat.pl
+++ actions/eat.pl
@@ -5,7 +5,7 @@ processEat(X, Y, NewX, NewY) :-
   YEaten is (Y+NewY)/2,
   convertCoordinate(XEaten, YEaten, Pos),
   b_getval(board, Board),
-  replace(Board, Pos, em, NewBoard)
+  replace(Board, Pos, em, NewBoard),
   b_setval(board, NewBoard).
 
 %doEat(Board, X, Y, NewX, NewY, NewBoard).
